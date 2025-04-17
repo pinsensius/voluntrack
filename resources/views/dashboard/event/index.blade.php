2e835d08105ce0f9751b35bd103e8e3ccb6f0125
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-04-17 09:44:42 +0700

    fix layout atur event blade

--- resources/views/dashboard/event/index.blade.php
+++ resources/views/dashboard/event/index.blade.php
@@ -34,10 +34,10 @@
             color: black;
         }
 
-        .button-action button:nth-child(3) {
+        /* .button-action button:nth-child(3) {
             background-color: #FF0000;
             color: white;
-        }
+        } */
     </style>
 
     <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -47,51 +47,54 @@
 
         <div class="Events d-flex flex-column gap-4 mt-5">
             @foreach ($events as $event)
-            <div class="row  w-100">
-                <div class="col d-flex justify-content-between align-items-center">
-                    <img src="{{ asset('storage/' . json_decode($event->event_image)[0]) }}" alt="profile" width="40">
-                    <h5>{{$event->nama}}</h5>
-                    <div class="status">
-                        <div class="round d-flex gap-2">
-                            @if($event->status == "pending")
-                            <svg width="20" height="20">
-                                <circle cx="10" cy="10" r="5" fill="yellow" />
-                            </svg>
-                            @elseif($event->status == "rejected")
-                            <svg width="20" height="20">
-                                <circle cx="10" cy="10" r="5" fill="red" />
-                            </svg>
-                            @elseif($event->status == "approved")
-                            <svg width="20" height="20">
-                                <circle cx="10" cy="10" r="5" fill="green" />
-                            </svg>
+                <div class="row  w-100">
+                    <div class="col d-flex justify-content-between align-items-center">
+                        <img src="{{ asset('storage/' . json_decode($event->event_image)[0]) }}" alt="profile"
+                            width="40">
+                        <h5>{{ $event->nama }}</h5>
+                        <div class="status">
+                            <div class="round d-flex gap-2">
+                                @if ($event->status == 'pending')
+                                    <svg width="20" height="20">
+                                        <circle cx="10" cy="10" r="5" fill="yellow" />
+                                    </svg>
+                                @elseif($event->status == 'rejected')
+                                    <svg width="20" height="20">
+                                        <circle cx="10" cy="10" r="5" fill="red" />
+                                    </svg>
+                                @elseif($event->status == 'approved')
+                                    <svg width="20" height="20">
+                                        <circle cx="10" cy="10" r="5" fill="green" />
+                                    </svg>
+                                @endif
+                                <h6 class="fw-bold">{{ $event->status }}</h6>
+                            </div>
+                        </div>
+                        <div class="button-action d-flex gap-4">
+                            @if (auth()->user()->canany(['admin-show']))
+                                <button class="rounded-3 px-4 py-2" style="width: 33%;">
+                                    <a href="{{ route('admin.event.show', $event->id_event) }}"
+                                        class="text-blue-600 dark:text-blue-400 hover:text-blue-500">View Details</a>
+                                </button>
                             @endif
-                            <h6 class="fw-bold">{{ $event->status}}</h6>
+                            @if (auth()->user()->canany(['admin-approve', 'admin-reject']))
+                                <form action="{{ route('admin.event.approve', $event->id_event) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <button type="submit"
+                                        class="rounded-3 px-4 py-2 bg-green-400 h-full">Izinkan</button>
+                                </form>
+                                <form action="{{ route('admin.event.reject', $event->id_event) }}" method="POST"
+                                    onsubmit="return confirm('Are you sure to reject this event?');">
+                                    @csrf
+                                    @method('PUT')
+                                    <button type="submit"
+                                        class="rounded-3 px-4 py-2 bg-red-400 h-full">Hentikan</button>
+                                </form>
+                            @endif
                         </div>
                     </div>
-                    <div class="button-action d-flex gap-4">
-                        @if (auth()->user()->canany(['admin-show']))
-                        <button class="rounded-3 px-4 py-2" style="width: 33%;">
-                            <a href="{{ route('admin.event.show', $event->id_event) }}"
-                                class="text-blue-600 dark:text-blue-400 hover:text-blue-500">View Details</a>
-                        </button>
-                        @endif
-                        @if (auth()->user()->canany(['admin-approve', 'admin-reject']))
-                        <form action="{{ route('admin.event.approve', $event->id_event) }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <button type="submit" class="rounded-3 px-4 py-2" style="width: 33%;">Izinkan</button>
-                        </form>
-                        <form action="{{ route('admin.event.reject', $event->id_event) }}" method="POST"
-                            onsubmit="return confirm('Are you sure to reject this event?');">
-                            @csrf
-                            @method('PUT')
-                            <button type="submit" class="rounded-3 px-4 py-2" style="width: 33%;">Hentikan</button>
-                        </form>
-                        @endif
-                    </div>
                 </div>
-            </div>
             @endforeach
         </div>
 
